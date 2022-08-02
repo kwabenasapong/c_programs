84f9e3d3d62d16e9d78da377f182ff51a4775a1d
Kwabena Sapong <kwabenasapong@gmail.com>
2022-08-02 20:53:44 +0000

    Example shell files

--- kb_shell/shell.h
+++ kb_shell/shell.h
@@ -5,5 +5,9 @@ void kbsh_loop(void);
 char *kbsh_readline(void);
 char **kbsh_split_line(char *line);
 int kbsh_start_process(char **args);
+int kbsh_cd(char **args);
+int kbsh_help(char **args);
+int kbsh_exit(char **args);
+int kbsh_builtins();
 
 #endif /* _SHELL_H_ */
